3e575d29a6bb3fbaaf5b132ff10f156cfc3b3e43
rishi9504 <hrishikesh.pandey9955@gmail.com>
2020-06-30 23:27:49 +0530

    module 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import './App.css'
-import './Quiz.js'
+
+import Quiz from './Quiz.js'
 
 class App extends Component {
   render() {
     return (
-      <Quiz></Quiz>
+      <Quiz/>
     )
   }
 }
