af9db29ae4cd19c45c8fcdae7a96128d230f4d5c
rishi9504 <hrishikesh.pandey9955@gmail.com>
2020-07-01 20:33:45 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import './App.css'
+import {Hero} from './Hero.jsx'
 
 import Quiz from './Quiz.js'
 
 class App extends Component {
   render() {
     return (
+      <div>
+      <Hero/>
       <Quiz/>
+      </div>
     )
   }
 }
---
+++ src/Hero.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Hero = () =>
+<div className="row">
+	<div className="jumbotron col-6 offset-1">
+		<h1> Quiz</h1>
+		<p>Select the correct answer from the given options</p>
+	</div>
+</div>
